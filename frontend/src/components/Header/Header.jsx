d0738ea8e8131f859fd29a6e34ccb895108721e1
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-13 15:47:03 +0530

    updated Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -3,10 +3,34 @@ import logo from '../../assets/images/logo.png'
 import userImg from '../../assets/images/avatar-icon.png'
 import { NavLink, Link } from 'react-router-dom'
 import { BiMenu } from 'react-icons/bi'
+import { useEffect, useRef } from 'react'
 
 const Header = () => {
+  const headerRef = useRef(null)
+  const menuRef = useRef(null)
+
+  const handleStickyHeader = () => {
+    window.addEventListener('scroll', () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add('sticky_header')
+      } else {
+        headerRef.current.classList.remove('sticky_header')
+      }
+    })
+  }
+
+  const toggleMenu = () => menuRef.current.classList.toggle('show_menu')
+
+  useEffect(() => {
+    handleStickyHeader()
+    return () => window.removeEventListener('scroll', handleStickyHeader)
+  })
+
   return (
-    <header className="header flex items-center">
+    <header className="header flex items-center" ref={headerRef}>
       <div className="container">
         <div className="flex items-center justify-between">
           {/* Logo */}
@@ -15,7 +39,7 @@ const Header = () => {
           </div>
 
           {/* Menu */}
-          <div className="navigation">
+          <div className="navigation" ref={menuRef} onClick={toggleMenu}>
             <ul className="menu flex items-center gap-[2.7rem]">
               {navLinks.map((link, i) => {
                 return (
@@ -38,7 +62,7 @@ const Header = () => {
 
           {/* Menu - Right */}
           <div className="flex items-center gap-4">
-            <div className="">
+            <div className="hidden">
               <Link to="/">
                 <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
                   <img
@@ -59,7 +83,7 @@ const Header = () => {
               </button>
             </Link>
 
-            <span className="md:hidden">
+            <span className="md:hidden" onClick={toggleMenu}>
               <BiMenu className="w-6 h-6 cursor-pointer" />
             </span>
           </div>
