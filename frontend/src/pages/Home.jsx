59ca3097c5465274745876efdaa1600174d9d46b
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-13 17:12:32 +0530

    added services section to the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,12 @@
 import heroImg1 from '../assets/images/hero-img01.png'
 import heroImg2 from '../assets/images/hero-img02.png'
 import heroImg3 from '../assets/images/hero-img03.png'
+import icon1 from '../assets/images/icon01.png'
+import icon2 from '../assets/images/icon02.png'
+import icon3 from '../assets/images/icon03.png'
+
+import { Link } from 'react-router-dom'
+import { BsArrowRight } from 'react-icons/bs'
 
 const Home = () => {
   return (
@@ -74,7 +80,89 @@ const Home = () => {
       </section>
       {/* Hero Section Ends */}
 
-      
+      {/* Services Section Starts */}
+      <section>
+        <div className="container">
+          <div className="lg:w-[470px] mx-auto">
+            <h2 className="heading text-center">
+              Providing the best medical services
+            </h2>
+            <p className="text_para text-center">
+              World-class care for everyone. Our booking system offers unmatched
+              health care.
+            </p>
+          </div>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]">
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon1} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Find a Doctor
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
+                  autem ut deleniti voluptas quia veritatis, est.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] 
+                mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon2} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Find a Clinic
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
+                  autem ut deleniti voluptas quia veritatis, est.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] 
+                mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+
+            <div className="py-[30px] px-5">
+              <div className="flex items-center justify-center">
+                <img src={icon3} alt="" />
+              </div>
+              <div className="mt-[30px]">
+                <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+                  Book an Appointment
+                </h2>
+                <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse
+                  autem ut deleniti voluptas quia veritatis, est.
+                </p>
+                <Link
+                  to="/doctors"
+                  className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] 
+                mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  <BsArrowRight className="group-hover:text-white w-6 h-5" />
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Services Section Ends */}
     </>
   )
 }
