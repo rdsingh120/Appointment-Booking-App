a42c88c5328b7cb78ab6517bad01310b8e33242f
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-14 22:09:45 +0530

    added Feature section to the homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import heroImg3 from '../assets/images/hero-img03.png'
 import icon1 from '../assets/images/icon01.png'
 import icon2 from '../assets/images/icon02.png'
 import icon3 from '../assets/images/icon03.png'
+import featureImg from '../assets/images/feature-img.png'
+import videoIcon from '../assets/images/video-icon.png'
+import avatarIcon from '../assets/images/avatar-icon.png'
 
 import { Link } from 'react-router-dom'
 import { BsArrowRight } from 'react-icons/bs'
@@ -182,6 +185,74 @@ const Home = () => {
         </div>
       </section>
       {/* Medical Services Section Ends */}
+
+      {/* Feature Section Starts */}
+      <section>
+        <div className="container">
+          <div className="flex items-center justify-between flex-col lg:flex-row">
+            {/* Content */}
+            <div className="xl:w-[670px]">
+              <h2 className="heading">
+                Get Online Treatment <br /> Anytime you want
+              </h2>
+              <ul className="pl-4">
+                <li className="text_para">
+                  1. Schedule the appointment directly
+                </li>
+                <li className="text_para">
+                  2. Find a doctor here and contact their office directly.
+                </li>
+                <li className="text_para">
+                  3. Use our online booking tool to find out which clinics are
+                  accepting new patients.
+                </li>
+              </ul>
+              <Link to="/">
+                <button className="btn">Learn More</button>
+              </Link>
+            </div>
+            {/* Image */}
+            <div className="relative z-10 xl:w-[770px] flex justify-end mt-[50px] lg:mt-0">
+              <img src={featureImg} className="w-3/4" alt="feature-image" />
+              <div
+                className="w-[150px] lg:w-[248px] bg-white absolute bottom-[50px] left-0 
+              md:bottom-[100px] md:left-5 z-20 p-2 pb-3 lg:px-4 lg:pb-[26px] rounded-[10px]"
+              >
+                <div className="flex items-center justify-between">
+                  <div className="flex items-center gap-[6px] lg:gap-3">
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-headingColor font-[600]">
+                      Tue, 24
+                    </p>
+                    <p className="text-[10px] leading-[10px] lg:text-[14px] lg:leading-5 text-textColor font-[400]">
+                      10:00 AM
+                    </p>
+                  </div>
+                  <span
+                    className="w-5 h-5 lg:w-[34px] lg:h-[34px] flex items-center justify-center 
+                  bg-yellowColor rounded py-1 px-[6px] lg:py-3 lg:px-[9px]"
+                  >
+                    <img src={videoIcon} alt="video-icon" />
+                  </span>
+                </div>
+
+                <div
+                  className="w-[92px] lg:w-[133px] bg-[#CCF0F3] py-1 px-2 lg:py-[6px] 
+                lg:px-[10px] text-[8px] leading-[8px] lg:text-[12px] lg:leading-4 
+                text-irisBlueColor font-[500] mt-2 lg:mt-4 rounded-full"
+                >
+                  Online Consultation
+                </div>
+
+                <div className="flex items-center gap-[6px] lg:gap-[10px] mt-2 lg:mt-[18px]">
+                  <img src={avatarIcon} alt="avatar-icon" />
+                  <h4 className="text-[10px] leading-3 lg:text-[16px] lg:leading-[22px] font-[700] text-headingColor">Dr. Philip</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Feature Section Ends */}
     </>
   )
 }
