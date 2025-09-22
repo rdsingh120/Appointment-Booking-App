57c9f067ec8bac47391e8a498405d177c5c84967
Ripudaman Singh <rdsingh120@gmail.com>
2025-09-22 19:46:37 +0530

    add footer section

---
+++ frontend/src/assets/data/footerLinks.js
@@ -0,0 +1,70 @@
+import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai'
+import { RiLinkedinFill } from 'react-icons/ri'
+
+export const socialLinks = [
+  {
+    path: 'https://www.youtube.com',
+    icon: AiFillYoutube,
+  },
+  {
+    path: 'https://www.github.com',
+    icon: AiFillGithub,
+  },
+  {
+    path: 'https://www.instagram.com',
+    icon: AiOutlineInstagram,
+  },
+  {
+    path: 'https://www.linkedin.com',
+    icon: RiLinkedinFill,
+  },
+]
+
+export const quickLinks = [
+  {
+    path: '/home',
+    text: 'Home',
+  },
+  {
+    path: '/',
+    text: 'About us',
+  },
+  {
+    path: '/services',
+    text: 'Services',
+  },
+  {
+    path: '/',
+    text: 'Blog',
+  },
+]
+
+export const quickLinks2 = [
+  {
+    path: '/find-a-doctor',
+    text: 'Find A doctor',
+  },
+  {
+    path: '/',
+    text: 'Book an appointment',
+  },
+  {
+    path: '/',
+    text: 'Find a clinic',
+  },
+  {
+    path: '/',
+    text: 'Get a quick opinion',
+  },
+]
+
+export const quickLinks3 = [
+  {
+    path: '/',
+    text: 'Donate',
+  },
+  {
+    path: '/contact',
+    text: 'Contact us',
+  },
+]
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,93 @@
+import { Link } from 'react-router-dom'
+import logo from '../../assets/images/logo.png'
+import {
+  quickLinks,
+  quickLinks2,
+  quickLinks3,
+  socialLinks,
+} from '../../assets/data/footerLinks'
+
 const Footer = () => {
+  const year = new Date().getFullYear()
   return (
-    <div>Footer</div>
+    <footer className="pb-16 pt-10">
+      <div className="container">
+        <div className="flex justify-between flex-col md:flex-row flex-wrap gap-[30px]">
+          <div className="">
+            <img src={logo} alt="" />
+            <p className="text-[16px] leading-7 font-[400] text-textColor">
+              Copyright © {year}{' '}
+            </p>
+            <div className="flex items-center gap-1 mt-4">
+              {socialLinks.map((link, i) => (
+                <a
+                  href={link.path}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  key={i}
+                  className="w-9 h-9 border border-solid border-[#181A1E] rounded-full 
+                flex items-center justify-center group hover:bg-primaryColor hover:border-none"
+                >
+                  {<link.icon />}
+                </a>
+              ))}
+            </div>
+          </div>
+
+          <div className="">
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              Quick Links
+            </h2>
+            <ul>
+              {quickLinks.map((link, i) => (
+                <li key={i} className="mb-4">
+                  <Link
+                    to={link.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="">
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">
+              I want to
+            </h2>
+            <ul>
+              {quickLinks2.map((link, i) => (
+                <li key={i} className="mb-4">
+                  <Link
+                    to={link.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="">
+            <h2 className="text-[20px] leading-[30px] font-[700] mb-6 text-headingColor">Contact us</h2>
+            <ul>
+              {quickLinks3.map((link, i) => (
+                <li key={i} className="mb-4">
+                  <Link
+                    to={link.path}
+                    className="text-[16px] leading-7 font-[400] text-textColor"
+                  >
+                    {link.text}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 export default Footer
